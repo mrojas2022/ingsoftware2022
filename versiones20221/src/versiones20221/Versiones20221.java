c3a3096e559e6f7d4d62af952a78699f61d557b7
Mauricio <Mauricio@192.168.100.4>
2022-06-07 15:35:21 -0500

    version 2 adicione mensaje versi{on 2

--- versiones20221/src/versiones20221/Versiones20221.java
+++ versiones20221/src/versiones20221/Versiones20221.java
@@ -16,6 +16,7 @@ public class Versiones20221 {
      */
     public static void main(String[] args) {
         System.out.println("version 1 2022");
+        System.out.println("versión 2");
     }
     
 }
