286db425fdfe4d17691dea98fb820814433578c0
Mauricio <Mauricio@192.168.100.4>
2022-06-07 15:17:12 -0500

    version 1 2022-1

---
+++ versiones20221/src/versiones20221/Versiones20221.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package versiones20221;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Versiones20221 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("version 1 2022");
+    }
+    
+}
